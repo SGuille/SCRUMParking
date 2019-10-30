a8f3a60663aa1b6807bdd932a0cda6946dd7d256
GonzaloMolina <gonza.molinad@gmail.com>
2019-10-29 21:45:46 -0300

    el proyecto corre en teoria

--- SCRUMParkingProyect/src/App.js
+++ SCRUMParkingProyect/src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Hola!</h2>
-        </div>
-        <p className="App-intro">
-          Hello Electron!
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
-  }
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Funciona o no?
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
